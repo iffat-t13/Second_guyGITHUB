5b11faf74bba4561eb56accd341a5f6587b59fc0
iffat_t13 <iffat.jabin@gmail.com>
2019-07-02 21:44:50 -0400

    second guy first commit

---
+++ SecondProject/src/secondpackage/Secondclass.java
@@ -0,0 +1,11 @@
+package secondpackage;
+
+public class Secondclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My testcase by second guy");
+
+	}
+
+}
