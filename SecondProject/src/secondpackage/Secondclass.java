ab1a3c7fc5d987777354b738f6e7e7c4026b5b06
iffat_t13 <iffat.jabin@gmail.com>
2019-07-02 21:55:22 -0400

    first guy commits

--- SecondProject/src/secondpackage/Secondclass.java
+++ SecondProject/src/secondpackage/Secondclass.java
@@ -4,7 +4,7 @@ public class Secondclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My testcase by second guy");
+		System.out.println("My testcase by second guy_updated by first guy");
 
 	}
 
